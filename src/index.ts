1ff60f61e37597ad79ceaa3eac713e2b01d181b9
mdmrk <mariodavo.20@gmail.com>
2025-12-07 14:40:53 +0100

    remove old files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
 import { bodyId, devMode } from "@/config"
-import { BlockCounter } from "@/lib/block-counter"
-import { getCurrentPage } from "@/lib/get-current-page"
-import { makeNavbarSticky } from "@/lib/make-navbar-sticky"
-import { MenuButtonsInjector } from "@/lib/menu-buttons-injector"
-import { onReadyForScripting } from "@/lib/on-ready-for-scripting"
-import { registerAutoReloadAfterIdle } from "@/lib/register-auto-reload-after-idle"
-import { runFeedsCleaner } from "@/lib/run-feeds-cleaner"
-import { SettingsMenuInjector } from "@/lib/settings-menu-injector"
-import { updateThemeConfigWhenPossible } from "@/lib/updateThemeConfigWhenPossible"
 import STYLES from "@/style.css"
-import { injectConsole } from "@/utils/inject-console"
-import { removeAppInstallPrompt } from "@/lib/remove-app-install-prompt"
 ;(() => {
 	// Make sure this is the React-Mobile version of facebook
 	if (document.body.id !== bodyId) {
@@ -18,42 +7,17 @@ import { removeAppInstallPrompt } from "@/lib/remove-app-install-prompt"
 		return
 	}
 
-	injectConsole("FCX (UserScript)")
 	GM_addStyle(STYLES)
 
-	onReadyForScripting(() => {
-		console.log("Ready for scripting")
+	console.log("Ready for scripting")
 
-		removeAppInstallPrompt()
-		// Store all abort functions
-		const aborts: Array<() => void> = [
-			updateThemeConfigWhenPossible(),
-			// Show counter on top
-			...(devMode ? [BlockCounter.getInstance().register()] : []),
+	// Store all abort functions
+	const aborts: Array<() => void> = []
 
-			// Inject menu buttons and settingsMenu only if we are on the feed page
-			...(getCurrentPage() === "feed"
-				? [
-						// Inject menu buttons [settings, feed]
-						MenuButtonsInjector.getInstance().inject(),
-						// Setup settingsMenu listeners and return cleanup function
-						SettingsMenuInjector.getInstance().inject(),
-					]
-				: []),
-
-			// Main cleaner
-			runFeedsCleaner(),
-			// Auto reload after idle
-			registerAutoReloadAfterIdle(),
-		]
-
-		makeNavbarSticky()
-
-		return () => {
-			console.log("Not Ready for scripting")
-			// Cleanup code like removing dom nodes and destroying event listeners
-			aborts.forEach(abort => abort?.())
-			aborts.length = 0
-		}
-	})
+	return () => {
+		console.log("Not Ready for scripting")
+		// Cleanup code like removing dom nodes and destroying event listeners
+		aborts.forEach(abort => abort?.())
+		aborts.length = 0
+	}
 })()
--- src/lib/get-own-language-filters.ts
+++
@@ -1,4 +0,0 @@
-import type { KeywordsPerLanguageItem } from "@/data/keywords-per-language"
-
-export const getOwnLangFilters = (obj: KeywordsPerLanguageItem) =>
-	navigator.languages.flatMap(lang => obj[lang])
